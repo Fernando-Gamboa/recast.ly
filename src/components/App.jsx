66dfb04df0d29ef0f570729ecaf66c26b6a0a10f
Fernando <gamboaf011@gmail.com>
2022-10-01 23:02:37 -0700

    Completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,70 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoList from './VideoList.js';
+import VideoListEntry from './VideoListEntry.js';
+import VideoPlayer from './VideoPlayer.js';
+import Search from './Search.js';
+import searchYouTube from '../lib/searchYouTube.js';
+
+var App = () => {
+
+  const { useState, useEffect } = React;
+  // initial video in useState is first video
+  const [listOfVideos, setlistOfVideos] = useState([]);
+  const [videoSelected, setVideoSelected] = useState(exampleVideoData[0]);
+  // create helper function
+  // useEffect helper function ---------------------
+  // WILL RUN ON PAGE LOAD
+  useEffect(() => {
+    searchYouTube('OpTic Yay', function(data) {
+      setlistOfVideos(data);
+      setVideoSelected(data[0]);
+    });
+  }, []);
+
+  var selectedVideo = (video) => { // selectedVideo helper function -----------------
+    // setVideoSelcted is setting state of each video being selected
+    setVideoSelected(video);
+  };
+
+  var searchVideo = (query) => { // selectedVideo helper function -----------------
+    // setVideoSelcted is setting state of each video being selected
+    searchYouTube(query, function(data) {
+      setlistOfVideos(data);
+      setVideoSelected(data[0]); //nice work
+    });
+  };
+
+
+  // search youtube function and pass it down to searchbar
+  // on change, run function everytime on current entry
+  // useeffect -> videolist
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <div><h5><em>search</em> view goes here</h5>
+            <Search search={videoSelected} searchVideo={searchVideo}/>
+          </div>
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <div>
+            <h5><em>videoPlayer</em> view goes here</h5>
+            <VideoPlayer video={videoSelected}/>
+          </div>
+        </div>
+        <div className="col-md-5">
+          <div>
+            <h5><em>videoList</em> view goes here</h5>
+            <VideoList videos={listOfVideos} selectedVideo={selectedVideo}/>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -6,6 +6,15 @@ $.ajaxPrefilter(function (settings, _, jqXHR) {
 
 var searchYouTube = (query, callback) => {
   // TODO
+  $.ajax({
+    url: 'https://app-hrsei-api.herokuapp.com/api/recastly/videos',
+    type: 'GET',
+    data: {q: query,
+      youtube_api_key: YOUTUBE_API_KEY},
+    contentType: 'application/json',
+    success: callback,
+    error: console.log('Error'),
+  });
 };
 
 export default searchYouTube;
